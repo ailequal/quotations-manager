62b9c2cbb4b87e8e3d6c0076e353b2867f18963b
ailequal <37016865+ailequal@users.noreply.github.com>
2020-07-15 10:33:11 +0200

    Aggiunto piccolo box notifica quando si attiva il plugin

--- quotations-manager/quotations-manager.php
+++ quotations-manager/quotations-manager.php
@@ -1,35 +1,55 @@
 <?php
 /*
-Plugin Name: Quotations Manager
-Plugin URI: http://wordpress.org/plugins/quotations-manager/
-Description: Un plugin per WordPress per gestire la creazione e modifica di servizi, comprese l'aggiunta di eventuali opzioni extra.
-Author: ailequal
-Version: 0.0.1
-Author URI: https://github.com/ailequal
+ * Plugin Name: Quotations Manager
+ * Plugin URI: http://wordpress.org/plugins/quotations-manager/
+ * Description: Un plugin per WordPress per gestire la creazione e modifica di
+ * servizi, comprese l'aggiunta di eventuali opzioni extra.
+ * Author: ailequal
+ * Version: 0.0.1
+ * Author URI: https://github.com/ailequal
 */
 
 
 /*
- * Il codice e' attualmente scritto in maniera procedurale, ma sara' successivamente rivisto in OOP
- * Il plugin utilizza in tutti i file e funzioni il prefisso "quoma"
+ * Il codice e' attualmente scritto in maniera procedurale, ma sara'
+ * successivamente riscritto in OOP con "wppb.me" come boilerplate.
+ * Il plugin utilizza in tutti i file e funzioni il prefisso "quoma".
  */
 
 
-/*
- * register_activation_hook()
+/**
+ * Il codice che si avvia all'attivazione del plugin
  */
 
-register_activation_hook( __FILE__, 'quoma_install' );
 function quoma_install() {
 	global $wp_version;
 	if ( version_compare( $wp_version, '5.4.2', '<' ) ) {
-		wp_die( 'This plugin requires WordPress version 5.4.2 or higher.' );
+		wp_die( 'Questo plugin richiede la versione di WordPress 5.4.2 o superiore.' );
+	}
+	set_transient( 'quoma_admin_notices_transient', true, 5 );
+}
+
+register_activation_hook( __FILE__, 'quoma_install' );
+
+/**
+ * Messaggio di notifica di creazione delle pagine all'attivazione del plugin
+ */
+function quoma_admin_notices() {
+	if ( get_transient( 'quoma_admin_notices_transient' ) ) {
+		?>
+		<div class="notice notice-info is-dismissible">
+			<p><?php _e( 'Quotations Manager ha generato le seguenti pagine: "Accesso negato", "I miei preventivi" e "Servizi".', 'quotations-manager' ); ?></p>
+		</div>
+		<?php
+		delete_transient( 'quoma_admin_notices_transient' );
 	}
 }
 
+add_action( 'admin_notices', 'quoma_admin_notices' );
 
-/*
- * include
+
+/**
+ * Tutti i file necessari per il funzionamente del plugin
  */
 
 // Creazione delle pagine
@@ -51,11 +71,12 @@ include( plugin_dir_path( __FILE__ ) . 'includes/quoma-menu.php' );
 include( plugin_dir_path( __FILE__ ) . 'includes/quoma-ajax.php' );
 
 
-/*
- * register_deactivation_hook()
+/**
+ * Il codice che si avvia alla disattivazione del plugin
  */
 
-register_deactivation_hook( __FILE__, 'prowp_deactivate()' );
 function prowp_deactivate() {
 	// azioni da definire
 }
+
+register_deactivation_hook( __FILE__, 'prowp_deactivate()' );
